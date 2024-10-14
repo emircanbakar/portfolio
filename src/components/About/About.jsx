485ba0b3b2055e8f70157e20bd6096e50fd7641d
emircanbakar <emirbakar952@gmail.com>
2024-10-14 04:44:33 +0300

    about kısmı geliştirmeye devam

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,30 +1,17 @@
 import "./About.css";
+import comp from "../../assets/comp.png";
 
 const About = () => {
   return (
-    <div className="about-container">
+    <div className="about">
       <span className="about-title"> {"<about>"} </span>
-      <div className="about-section">
-        <p className="about-text">
+      <div className="about-container">
+        <span className="about-text">
           {
             "I'm a Frontend Developer who designs simple, impactful, and functional web interfaces tailored to users' needs. I focus on delivering aesthetic and intuitive web experiences by providing creative solutions. I stay up-to-date with technology, pay attention to details, and strive to achieve better results with every project."
           }
-        </p>
-        <div className="about-cards">
-          <div className="card">HTML</div>
-          <div className="card">CSS</div>
-          <div className="card">JAVASCRIPT</div>
-          <div className="card">REACT.JS</div>
-          <div className="card">TAILWINDCSS</div>
-          <div className="card">SHADCNUI</div>
-          <div className="card">MUI</div>
-          <div className="card">SASS</div>
-          <div className="card">SCSS</div>
-          <div className="card">TYPESCRIPT</div>
-          <div className="card">NODE.JS</div>
-          <div className="card">NEXT.JS</div>
-          <div className="card">MONGODB</div>
-        </div>
+        </span>
+        <img src={comp} alt="skills" className="skill-comp" />
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,10 @@
-import { useState } from "react";
-import "./Header.css";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <div>
-      <nav className="navbar">
-        <div className="hamburger" onClick={toggleMenu}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
-      </nav>
-      <div className={`overlay ${isOpen ? "open" : ""}`}>
-        <ul className="overlay-links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-        <button className="close-btn" onClick={toggleMenu}>X</button>
-      </div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
