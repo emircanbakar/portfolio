629561551699c545ee52528e43ee6be7971d1a0f
emircanbakar <emirbakar952@gmail.com>
2025-04-13 23:41:51 +0300

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import "./App.css";
-import About from "./components/About";
-// import { useState } from "react";
-// import Loader from "./components/Loader/Loader";
+import About from "./components/About/About";
 
-function App() {
-  // const [loading, setLoading] = useState(true);
-  // const handleLoaderComplete = () => {
-  //   setLoading(false);
-  // };
-
+const App = () => {
   return (
-    <div className="flex flex-row text-white bg-black">
-      <About/>
+    <div className="flex flex-row items-center justify-between h-screen bg-black text-white max-w-[800px] max-h-[600px]: m-auto gap-16">
+      <div className="flex flex-col items-left justify-center">
+        <h2>Emircan Bakar</h2>
+        <h3>Software Engineer</h3>
+      </div>
+      <div>
+        <About />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
